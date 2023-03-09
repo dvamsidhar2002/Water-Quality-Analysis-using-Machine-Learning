94e2249273844a058681754686757beb99fe2c2f
D VAMSIDHAR <vamsidhard2002@gmail.com>
2023-03-09 13:54:30 +0530

    Update Water_Quality_Analysis_Web_App.py

--- Water_Quality_Analysis_Web_App.py
+++ Water_Quality_Analysis_Web_App.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model = pickle.load(open("D:\Supervised Machine Learning with Python (Streamlit Deployed)\Water Quality Analysis\Trained_model.sav",'rb'))
+loaded_model = pickle.load(open("Trained_model.sav",'rb'))
 
 #creating a function for prediction
 def water_quality_analyser(input_data):
